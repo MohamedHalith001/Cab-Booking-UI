5e4522dba4d51972abb888f318c7866c009b5465
MohamedHalith001 <69855107+MohamedHalith001@users.noreply.github.com>
2021-04-23 09:49:04 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function login() {
     event.preventDefault();
     let mobileNumber = document.getElementById("mobileNumber").value;
@@ -34,36 +33,3 @@ function checkPass(password) {
         return false;
     }
 }
-=======
-function login() {
-    event.preventDefault();
-    let mobile = document.getElementById("mobile");
-    let pass = document.getElementById("pass");
-    let mobileno = mobile.value;
-    let password = pass.value;
-    //Calling checkMobile and checkPass and executing commands based on result
-    if (checkMobile(mobileno) && checkPass(password)) {
-        alert("Login Successful");
-        window.location.replace('CabDetails.html');
-    } else {
-        alert("Enter correct username and password");
-    }
-};
-//Validates mobile no
-function checkMobile(mobileno) {
-    if (mobileno == 9524896959) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-//Validates password
-function checkPass(password) {
-    if (password.length >= 8) {
-        return true;
-    } else {
-        return false;
-    }
-}
->>>>>>> refs/remotes/origin/main
